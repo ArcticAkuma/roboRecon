e536cddea8eae181af1177a3adf5a9845d2056d5
Dominik Enders <dominik.enders46@gmail.com>
2025-01-07 16:53:46 +0100

    set old files to new branch

--- __old/config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-with open('config.yml', 'r') as file:
-    config = yaml.safe_load(file)
-
-    print(config['donkey-server']['hostname'])
-    print(config['donkey-server']['port'])
-    print(config['yolo']['console-logging'])
