f3796a6ee2e334a003facee2c63744a607ac7beb
Dominik Enders <dominik.enders46@gmail.com>
2025-01-07 21:07:32 +0100

    added comments to src

--- altered_files/donkeycar/parts/robo_recon.py
+++ altered_files/donkeycar/parts/robo_recon.py
@@ -4,6 +4,9 @@ from cv_bridge import CvBridge
 from tensorflow.python.ops.metrics_impl import true_positives
 
 ### SELF-MAINTAINED
+# Create DonkeyCar part to receive all necessary ROS data.
+# This part has to be run in a separate thread because of it being a rospy node.
+# To archive this, DonkeyCar's built in possibility to create 'threaded' parts can be utilized.
 
 class RosReceiver:
     def __init__(self):
@@ -13,28 +16,38 @@ class RosReceiver:
 
         self.bridge = CvBridge()
 
-        # todo: check if dummy defaults are needed
         self.depth_image = None
         self.rgb_image = None
 
+    # Will be executed on part initialize.
+    # ROS node is started here.
+    # Additionally, ROS subscribers are defined and created.
     def update(self):
         self.depth_subscriber = rospy.Subscriber('/camera/depth/image_rect_raw', Image, self.depth_frame_received)
         self.rgb_subscriber = rospy.Subscriber('/camera/color/image_raw', Image, self.rgb_frame_received)
 
         self.node = rospy.init_node('donkey_receiver', anonymous=True, disable_signals=True)
 
+    # Unused because of this being a 'threaded' part.
+    # See vehicle.py line update_parts().
     def run(self):
         return self.rgb_image, self.depth_image
 
+    # Returns transformed ROS data to DonkeyCar instance on update_parts() in vehicle.py.
     def run_threaded(self):
         return self.rgb_image, self.depth_image
 
+    # Will be executed on part shutdown.
+    # ROS node is shutdown here.
+    # Would happen on program exit anyway, just want to make sure.
     def shutdown(self):
         rospy.signal_shutdown('donkey shutdown')
         pass
 
+    # Transforms of sensor_msgs.msg.Image to further usable image/array.
     def depth_frame_received(self, msg):
         self.depth_image = self.bridge.imgmsg_to_cv2(msg)
 
+    # Transforms of sensor_msgs.msg.Image to further usable image/array.
     def rgb_frame_received(self, msg):
         self.rgb_image = self.bridge.imgmsg_to_cv2(msg)
--- src/override.py
+++ src/override.py
@@ -4,28 +4,36 @@ import os
 import shutil
 import sys
 
+# Localize project files on deployed system.
 PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
 ALTERED_PATH = os.path.join(PROJECT_PATH, '../altered_files/donkeycar')
 SCRIPT_PATH = os.path.join(PROJECT_PATH, '../scripts')
 
 logger = logging.getLogger(__name__)
 
-# 
+# Checks if application is run from within a virtual environment.
 def in_venv():
     return sys.prefix != sys.base_prefix
 
+# Checks if package exists within a current environment.
 def pkg_exists(name):
     return name in sys.modules or importlib.util.find_spec(name)
 
+# Runs overriding process for every file in sources_path.
+# All files will be copied to execution_path.
 def run_override(sources_path, execution_path):
     counter = 0
     new_folders = 0
     new_files = 0
     for path, sub_dirs, files in os.walk(sources_path):
         for name in files:
+            # Fetch full origin and target path.
             from_path = os.path.join(path, name)
             to_path = os.path.join(path, name).replace(sources_path, execution_path)
 
+            # Get directory of path.
+            # Check if directory exists at target location.
+            # If not prompt user to confirm process and create directory.
             dir = os.path.split(to_path)[0]
             if not os.path.exists(dir):
                 logger.warning(f"Folder does not exist in donkeycar: {dir}")
@@ -35,27 +43,34 @@ def run_override(sources_path, execution_path):
                 os.makedirs(dir)
                 new_folders += 1
 
+            # Copy file to target location.
             if not os.path.exists(to_path):
                 new_files += 1
             print(f"Coping {from_path} to {to_path}")
-            #shutil.copyfile(from_path, to_path)
+            shutil.copyfile(from_path, to_path)
             counter += 1
     return counter, new_folders, new_files
 
+
 def override_donkey():
+    # Check if application runs within a virtual environment.
+    # If not prompt user to confirm changes to global environment.
     if not in_venv():
         logger.warning("Not running inside of an virtual environment!")
         val = input("Continue anyway? [y/n]")
         if val != "y" and val != "yes":
             return
 
+    # Check if donkeycar package is installed in current environment.
     if not pkg_exists("donkeycar"): # todo: change to donkey
         logger.error("Donkeycar package not found in current environment!")
         return
 
+    # Fetch location of donkeycar package in current environment.
     import donkeycar
     donkey_path = os.path.dirname(os.path.realpath(donkeycar.__file__))
 
+    # Prompt user with warning.
     if (pkg_exists("pyfiglet")):
         from pyfiglet import Figlet
         f = Figlet(font='big')
@@ -70,9 +85,11 @@ def override_donkey():
     if val != "y" and val != "yes":
         return
 
+    # Run override process .
     counter, new_folders, new_files = run_override(ALTERED_PATH, donkey_path)
     counter2, new_folders2, new_files2 = run_override(SCRIPT_PATH, donkey_path + "/templates")
 
+    # Give user some data regarding process completion.
     print("\nOVERRIDE COMPLETE")
     print(f"This program altered {counter+counter2} file(s).")
     print(f"{new_folders+new_folders2} new folder(s) and {new_files+new_folders} new file(s) were created.")
